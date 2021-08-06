7a551c5aacdf9c466558afe7b69d8dadce8b18bf
Pathce <63540284+Pathce@users.noreply.github.com>
2021-08-06 18:00:26 +0900

    update

--- review.php
+++ review.php
@@ -6,7 +6,22 @@
     <link rel="stylesheet" href="./css/review.css" />
 </head>
 <body>
-
+    <div class="reviewInfo">
+        <table>
+            <tr>
+                <td>리뷰 제목</td>
+                <td id="reviewTitle">역시 실망시키지 않는 영화다</td>
+            </tr>
+            <tr>
+                <td>영화 제목</td>
+                <td id="movieTitle">재밌는 영화</td>
+            </tr>
+            <tr>
+                <td>평점</td>
+                <td id="score">10</td>
+            </tr>
+        </table>
+    </div>
     <script src="./js/review.js"></script>
 </body>
 </html>
