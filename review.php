9c23df0814da48a240b1006be194d388dd8eb507
Pathce <63540284+Pathce@users.noreply.github.com>
2021-08-06 17:55:49 +0900

    update

---
+++ review.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>내 정보</title>
+    <link rel="stylesheet" href="./css/review.css" />
+</head>
+<body>
+
+    <script src="./js/review.js"></script>
+</body>
+</html>
