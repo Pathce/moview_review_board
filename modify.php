499e9fb0ef6ed3972cd867f34296e86dd8ce9f34
Pathce <asfgzx@naver.com>
2021-08-09 20:45:18 +0900

    update

--- modify.php
+++ modify.php
@@ -2,10 +2,10 @@
 include $_SERVER['DOCUMENT_ROOT']."./db.php";
     $r_seq = $_GET['r_seq'];
     $sql = query("
-SELECT R.R_SEQ R_SEQ, R.R_SUBJECT R_TITLE, M.M_NAME M_TITLE, R.R_SCORE R_SCORE, R.R_TIMESTAMP R_DATE, R.R_REC R_REC
-FROM REVIEW R, MOVIE_INFO M
-WHERE R_SEQ='$r_seq' AND R.M_SEQ = M.M_SEQ");
-    print_r($sql->fetch_assoc());
+        SELECT R.R_SUBJECT R_TITLE, R.R_CONTENT R_CONTENT, M.M_NAME M_TITLE, R.R_SCORE R_SCORE
+        FROM REVIEW R, MOVIE_INFO M
+        WHERE R_SEQ='$r_seq' AND R.M_SEQ = M.M_SEQ");
+    $review = $sql->fetch_assoc();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,19 +28,19 @@ if (!isset($_SESSION['user_id'])) {
     <div id="write_area">
         <form action="modify_ok.php?r_seq=<?php echo $r_seq; ?>" method="post">
             <div id="in_title">
-                <input name="title" id="utitle" rows="1" cols="55" placeholder="리뷰 제목" maxlength="100" required>
+                <textarea name="r_title" id="utitle" rows="1" cols="55" placeholder="리뷰 제목" maxlength="100" rows="1" required><?php echo $review['R_TITLE']; ?></textarea>
             </div>
             <div class="wi_line"></div>
             <div id="in_name">
-                <input name="movie_title" id="mtitle" rows="1" cols="55" placeholder="영화 제목" maxlength="100" required>
+                <textarea name="m_title" id="mtitle" rows="1" cols="55" placeholder="영화 제목" maxlength="100" rows="1" required><?php echo $review['M_TITLE']; ?></textarea>
             </div>
             <div class="wi_line"></div>
             <div class="in_score">
-                <input name="movie_score" id="in_score" placeholder="평점">
+                <textarea name="r_score" id="in_score" placeholder="평점" rows="1"><?php echo $review['R_SCORE']; ?></textarea>
             </div>
             <div class="wi_line"></div>
             <div id="in_content">
-                <textarea name="content" id="ucontent" placeholder="내용" required></textarea>
+                <textarea name="r_content" id="ucontent" placeholder="내용" required><?php echo $review['R_CONTENT']; ?></textarea>
             </div>
             <div class="btn_sub">
                 <button type="submit">작성하기</button>
--- modify_ok.php
+++ modify_ok.php
@@ -1,1 +1,30 @@
 <?php
+include $_SERVER['DOCUMENT_ROOT']."./db.php";
+
+$r_seq = $_GET['r_seq'];
+$r_title = $_POST['r_title'];
+$m_title = $_POST['m_title'];
+$r_score = $_POST['r_score'];
+$r_content = $_POST['r_content'];
+
+print_r($r_seq);
+echo "<br>";
+print_r($r_title);
+echo "<br>";
+print_r($m_title);
+echo "<br>";
+print_r($r_score);
+echo "<br>";
+print_r($r_content);
+echo "<br>";
+
+$sql_movie = query("SELECT M_SEQ FROM MOVIE_INFO WHERE M_NAME='$m_title'");
+$m_seq = $sql_movie->fetch_assoc()['M_SEQ'];
+
+$sql_review = query("UPDATE REVIEW 
+SET M_SEQ="."$m_seq".", R_SUBJECT='$r_title', R_CONTENT='$r_content', R_SCORE=".$r_score."
+WHERE R_SEQ="."$r_seq");
+?>
+
+<script type="text/javascript">alert("수정되었습니다.");</script>
+<meta http-equiv="refresh" content="0 url=./review.php?r_seq=<?php echo $r_seq ?>">
