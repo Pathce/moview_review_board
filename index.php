d747cd5e5599cbdbede8321fe84f039818765d42
Pathce <63540284+Pathce@users.noreply.github.com>
2021-08-12 17:48:01 +0900

    update

--- index.php
+++ index.php
@@ -4,24 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <title>로그인</title>
+    <link rel="stylesheet" href="./css/index.css" >
 </head>
 <body>
+<div class="login_container">
     <h1>로그인</h1>
-    <hr />
     <?php if(!isset($_SESSION['user_id']) || !isset($_SESSION['user_name'])) { ?>
-        <form method="post" action="login_ok.php">
-            <p>ID : <input type="text" name="user_id" /></p>
-            <p>PW : <input type="password" name="user_pw" /></p>
-            <p><input type="submit" value="로그인" /></p>
-            <p><a href="signin.php">회원가입</a></p>
+        <form id="login_form" method="post" action="login_ok.php">
+            <p><input id="input_id" type="text" name="user_id" placeholder="ID"/></p>
+            <p><input id="input_pw" type="password" name="user_pw" placeholder="PW" /></p>
+            <p><input id="login_submit" type="submit" value="로그인" /><a href="main.php"><button id="login_cancel" type="button">취소</button></a></p>
+            <p><a id="sign_up" href="signin.php">회원가입</a></p>
         </form>
-    <?php
-        } else {
+        <?php
+    } else {
         $user_id = $_SESSION['user_id'];
         $user_name = $_SESSION['user_name'];
         echo "<p><strong>($user_name)님은 이미 로그인하고 있습니다.</strong>";
         echo "<a href='main.php'>[돌아가기]</a>";
         echo "<a href='logout.php'>[로그아웃]</a></p>";
     } ?>
+</div>
 </body>
 </html>
--- signin.php
+++ signin.php
@@ -33,51 +33,52 @@ echo "<br>";
     <link rel="stylesheet" type="text/css" href="./css/signin.css" />
 </head>
 <body>
+<div class="signin_container">
+    <h1>회원가입</h1>
     <form method="post" name="signin_form" action="signin_ok.php">
-        <fieldset>
-            <legend>Sign In</legend>
-            <table>
-                <tr>
-                    <th>ID</th>
-                    <td> : <input type="text" name="id" id="uid" required value=<?php echo $id; ?>></td>
-                    <td><button type="submit" id="btn_id_chk" formaction="id_chk.php" formmethod="post">확인</button></td>
-                    <input class="hidden" id="id_check" name="id_check" value=<?php echo $id_check; ?>>
-                    <td id="id_chk_ok" <?php if(!$id_check) echo 'class="hidden"'; ?>>사용 가능한 ID 입니다.</td>
-                </tr>
-                <tr>
-                    <th>PW</th>
-                    <td> : <input type="password" name="pw" id="upw" required value=<?php echo $pw; ?>></td>
-                </tr>
-                <tr>
-                    <th>PW 확인</th>
-                    <td> : <input type="password" name="rpw" id="r_upw" required value=<?php echo $rpw; ?>></td>
-                    <td id="eql" class="hidden">사용 가능합니다.</td>
-                    <td id="n_eql" class="">비밀번호가 일치하지 않습니다.</td>
-                    <input class="hidden" id="pw_check" name="pw_check" value=<?php echo $pw_check; ?>>
-                </tr>
-                <tr>
-                    <th>NAME</th>
-                    <td> : <input type="text" name="name" id="uname" required value=<?php echo $name; ?>></td>
-                </tr>
-                <tr>
-                    <th>EMAIL</th>
-                    <td> :
-                        <input type="text" name="email" id="uemail_id" required value=<?php echo $email; ?>>
-                        @
-                        <select name="emadress" id="uemail_adr">
-                            <option value="example.com" <?php if($emadress == 'example.com') echo "SELECTED"; ?>>example.com</option>
-                            <option value="naver.com" <?php if($emadress == 'naver.com') echo "SELECTED"; ?>>naver.com</option>
-                            <option value="google.com" <?php if($emadress == 'google.com') echo "SELECTED"; ?>>google.com</option>
-                        </select>
-                    </td>
-                    <td><button type="submit" formaction="email_chk.php" formmethod="post">확인</button></td>
-                    <input class="hidden" id="email_check" name="email_check" value=<?php echo $email_check; ?>>
-                    <td id="email_chk_ok" <?php if(!$email_check || $email_check=="false") echo 'class="hidden"'; ?>>사용 가능한 Email 입니다.</td>
-                </tr>
-            </table>
-            <input type="submit" value="전송" >
-        </fieldset>
+        <table>
+            <tr>
+                <th>ID</th>
+                <td colspan="2"><input type="text" name="id" id="uid" required placeholder="ID" value='<?php echo $id; ?>'></td>
+                <td><button type="submit" id="btn_id_chk" formaction="id_chk.php" formmethod="post">확인</button></td>
+                <input class="hidden" id="id_check" name="id_check" value=<?php echo $id_check; ?>>
+                <td id="id_chk_ok" <?php if(!$id_check) echo 'class="hidden"'; ?>>사용 가능한 ID 입니다.</td>
+            </tr>
+            <tr>
+                <th>PW</th>
+                <td colspan="2"><input type="password" name="pw" id="upw" required placeholder="PW" value='<?php echo $pw; ?>'></td>
+            </tr>
+            <tr>
+                <th>PW 확인</th>
+                <td colspan="2"><input type="password" name="rpw" id="r_upw" required placeholder="PW 확인" value='<?php echo $rpw; ?>'></td>
+                <td id="eql" class="hidden">사용 가능합니다.</td>
+                <td id="n_eql" class="">비밀번호가 일치하지 않습니다.</td>
+                <input class="hidden" id="pw_check" name="pw_check" value=<?php echo $pw_check; ?>>
+            </tr>
+            <tr>
+                <th>NAME</th>
+                <td colspan="2"><input type="text" name="name" id="uname" required placeholder="이름" value='<?php echo $name; ?>'></td>
+            </tr>
+            <tr>
+                <th>EMAIL</th>
+                <td>
+                    <input type="text" name="email" id="uemail_id" required placeholder="이메일" value='<?php echo $email; ?>'>
+                    @
+                    <select name="emadress" id="uemail_adr">
+                        <option value="example.com" <?php if($emadress == 'example.com') echo "SELECTED"; ?>>example.com</option>
+                        <option value="naver.com" <?php if($emadress == 'naver.com') echo "SELECTED"; ?>>naver.com</option>
+                        <option value="google.com" <?php if($emadress == 'google.com') echo "SELECTED"; ?>>google.com</option>
+                    </select>
+                </td>
+                <td><button type="submit" id="btn_email_chk" formaction="email_chk.php" formmethod="post">확인</button></td>
+                <input class="hidden" id="email_check" name="email_check" value='<?php echo $email_check; ?>'>
+                <td id="email_chk_ok" <?php if(!$email_check || $email_check=="false") echo 'class="hidden"'; ?>>사용 가능한 Email 입니다.</td>
+            </tr>
+        </table>
+        <input id="btn_signin" type="submit" value="회원가입" >
+        <button id="btn_cancel" type="button">취소</button>
     </form>
+</div>
     <script src="./js/signin.js"></script>
 </body>
 </html>
